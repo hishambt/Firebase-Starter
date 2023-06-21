0b9815cf0864c0db3ba85c0e6f58a048207f9578
hishambt <hisham.buteen@gmail.com>
2023-06-21 16:32:47 +0300

    Co-authored-by: hishambt <hisham.buteen@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'pro-cv-390512',
+    appId: '1:60634711089:web:4a9561e703be8649b68110',
+    storageBucket: 'pro-cv-390512.appspot.com',
+    apiKey: 'AIzaSyCmYlXS_qLGqOeYxuYYQFin1Ia5XyrzKEY',
+    authDomain: 'pro-cv-390512.firebaseapp.com',
+    messagingSenderId: '60634711089',
+    measurementId: 'G-GN6DHHL9ML',
+  },
+};
