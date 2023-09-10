0394e356a7e3381d0e1f0200d437ffce51abbb7f
Ibrahim Najjar <ibrahimnajjar79@gmail.com>
2023-09-10 18:23:23 +0300

    enhancement menu

--- src/app/shell/side-navbar/side-navbar.component.ts
+++ src/app/shell/side-navbar/side-navbar.component.ts
@@ -13,14 +13,12 @@ export class SideNavbarComponent {
 	appPages: Array<IMenuItem> = appPages;
 	routeSubscription: Subscription = new Subscription();
 	router = inject(Router);
-	activeRoute: string = '';
 
 	ngOnInit(): void {
 		this.routeSubscription = this.router.events.subscribe((event) => {
 			if (event instanceof NavigationEnd) {
-				this.activeRoute = event.url;
 				this.resetMenu();
-				this.openActiveMenus();
+				this.openActiveMenus(event.url);
 			}
 		});
 	}
@@ -31,19 +29,15 @@ export class SideNavbarComponent {
 		});
 	}
 
-	openActiveMenus() {
+	openActiveMenus(url: string) {
 		this.appPages.forEach((page: IMenuItem) => {
 			if (page.children) {
 				page.children.forEach((sub: IMenuItem) => {
-					if (sub.url == this.activeRoute) {
+					if (sub.url == url) {
 						page.open = true;
 					}
 				});
 			}
 		});
-	}
-
-	isOneChildActive(children: IMenuItem[]) {
-		return children.some((page) => page?.url == this.activeRoute);
 	}
 }
