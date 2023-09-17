b71f33b0b0df04c4217c533f14bf33f78e9e5bad
hishambt <hisham.buteen@gmail.com>
2023-09-17 16:59:36 +0300

    refactor(firebase): moved to new account, configure local and tailwind production builds

--- projects/core-ui/src/app/shell/side-navbar/side-navbar.ts
+++ projects/core-ui/src/app/shell/side-navbar/side-navbar.ts
@@ -5,19 +5,30 @@ export interface IMenuItem {
 	open?: boolean;
 	active?: boolean;
 	children?: IMenuItem[];
-}export const appPages: Array<IMenuItem> = [
+}
+export const appPages: Array<IMenuItem> = [
 	{
-		title: 'Main',
-		icon: 'trash',
-		children: [{ title: 'Home', url: '/home', icon: 'home' }],
+		title: 'Home',
+		url: '/home',
+		icon: 'home',
+	},
+	{
+		title: 'Contacts',
+		url: '/contacts',
+		icon: 'contacts',
 	},
 	{
 		title: 'Test',
 		icon: 'home',
-		children: [{ title: 'Profile', url: '/profile', icon: 'heart' }],
+		children: [
+			{
+				title: 'Home',
+				url: '/home',
+				icon: 'home',
+			},
+			{ title: 'Profile', url: '/profile', icon: 'heart' },
+			{ title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
+			{ title: 'Archived', url: '/folder/archived', icon: 'archive' },
+		],
 	},
-	{ title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-	{ title: 'Archived', url: '/folder/archived', icon: 'archive' },
-	{ title: 'Trash', url: '/folder/trash', icon: 'trash' },
-	{ title: 'Spam', url: '/folder/spam', icon: 'warning' },
 ];
