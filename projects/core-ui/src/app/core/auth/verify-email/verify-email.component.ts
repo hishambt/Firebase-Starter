d76637053da19ff168ed4fd21f571828d3e6ca59
Hisham Buteen <hisham.buteen@nextcarehealth.com>
2023-09-21 16:22:41 +0300

    fix(auth): fix verify page

--- projects/core-ui/src/app/core/auth/verify-email/verify-email.component.ts
+++ projects/core-ui/src/app/core/auth/verify-email/verify-email.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { take, Subscription } from 'rxjs';
-import { authState, Auth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../../services/auth.service';
@@ -12,28 +11,21 @@ import { AuthUser } from '../../../shared/models/IUser.model';
 	styleUrls: ['./verify-email.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class VerifyEmailComponent implements OnInit {
+export class VerifyEmailComponent {
 	private authService = inject(AuthService);
-	private auth = inject(Auth);
 	private router = inject(Router);
 
 	signOut$: Subscription | null = null;
 	verifyEmail$: Subscription | null = null;
 
-	user: AuthUser = null;
-
-	ngOnInit(): void {
-		authState(this.auth)
-			.pipe(take(1))
-			.subscribe((user: AuthUser) => {
-				if (user) {
-					this.user = user;
-				}
-			});
-	}
+	user$ = this.authService.currentUserProfile$;
 
 	verifyEmail(): void {
-		this.verifyEmail$ = this.authService.sendVerificationEmail(this.user!).pipe(take(1)).subscribe();
+		this.verifyEmail$ = this.authService
+			.userProvider((user: AuthUser) => {
+				return this.authService.sendVerificationEmail(user!).pipe(take(1));
+			})
+			.subscribe();
 	}
 
 	logout(): void {
