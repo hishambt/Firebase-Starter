93637a1e85ce98583953fe78beab046b67625b61
Hisham Buteen <hisham.buteen@nextcarehealth.com>
2024-01-30 17:31:14 +0200

    chore(core): updated commit script
    Description
    
    FS-FS-49

--- commit.mjs
+++ commit.mjs
@@ -1,5 +1,5 @@
 import { createPromptModule } from 'inquirer';
-import { execSync } from 'child_process';
+import { exec, execSync } from 'child_process';
 
 const scopes = ['core', 'package', 'auth', 'profile', 'shared', 'library'];
 
@@ -20,98 +20,109 @@ const types = [
 const libraryScopes = ['element', 'component', 'shared', 'utils'];
 
 const prompt = createPromptModule();
-prompt([
-	{
-		type: 'list',
-		name: 'type',
-		message: 'Change Type:',
-		choices: types,
-	},
-	{
-		type: 'list',
-		name: 'scope',
-		message: 'Scope of changes:',
-		choices: scopes,
-	},
-	{
-		type: 'list',
-		name: 'specificScope',
-		message: 'Choose a specific scope',
-		choices: libraryScopes,
-		when: (answers) => answers.scope == 'library',
-	},
-	{
-		type: 'input',
-		name: 'message',
-		message: 'Commit message:',
-		validate: (input) => {
-			if (!!input) {
-				return true;
-			} else {
-				return 'Please enter a commit message';
-			}
-		},
-		filter: (input) => input.charAt(0).toLowerCase() + input.slice(1),
-	},
-	{
-		type: 'input',
-		name: 'jiraNumber',
-		message: 'JIRA Ticket Number: (FS-{number})',
-		validate: (input) => {
-			if (!!input && input.match(/^\d+$/)) {
-				return true;
-			} else {
-				return 'Please enter a valid Jira number';
-			}
-		},
-	},
-	{
-		type: 'input',
-		name: 'description',
-		message: 'Detailed description:',
-	},
-	{
-		type: 'confirm',
-		name: 'breakingChange',
-		default: false,
-		message: 'Is it a breaking change?',
-	},
-	{
-		type: 'confirm',
-		name: 'addFiles',
-		message: 'Add all files automatically?',
-	},
-]).then((answers) => {
-	if (answers.breakingChange) {
-		console.log('Breaking Change Not Implemented');
-	}
 
-	const scope = answers.specificScope ? answers.specificScope : answers.scope;
-	const description = answers.description || '';
+exec('git branch --show-current', (err, stdout, stderr) => {
+	const branchSplitted = stdout.split('-');
+	const jiraTicketId = [branchSplitted[0], branchSplitted[1]].join('-');
+	startPrompt(jiraTicketId);
+});
 
-	const commitCommand = `git commit -m "${answers.type}(${scope}): ${answers.message}" -m "${description}" -m "FS-${answers.jiraNumber}"`;
-	//chore(package): update author information Update the author field in package.json to "Softside - Angular Firebase Starter". Refs FS-49
-
-	if (answers.addFiles) {
-		execSync('git add .', { stdio: 'inherit' });
-	}
-
-	try {
-		execSync(commitCommand, { stdio: 'inherit' });
-	} catch (e) {
-		console.error(e.message);
-		process.exit(1);
-	}
-
+function startPrompt(jiraTicketId) {
 	prompt([
 		{
+			type: 'list',
+			name: 'type',
+			message: 'Change Type:',
+			choices: types,
+		},
+		{
+			type: 'list',
+			name: 'scope',
+			message: 'Scope of changes:',
+			choices: scopes,
+		},
+		{
+			type: 'list',
+			name: 'specificScope',
+			message: 'Choose a specific scope',
+			choices: libraryScopes,
+			when: (answers) => answers.scope == 'library',
+		},
+		{
+			type: 'input',
+			name: 'message',
+			message: 'Commit message:',
+			validate: (input) => {
+				if (!!input) {
+					return true;
+				} else {
+					return 'Please enter a commit message';
+				}
+			},
+			filter: (input) => input.charAt(0).toLowerCase() + input.slice(1),
+		},
+		{
+			type: 'input',
+			name: 'jiraNumber',
+			message: 'JIRA Ticket Number: (FS-{number})',
+			default: jiraTicketId,
+			validate: (input) => {
+				if (!!input && input.match(/.+-\d+$/)) {
+					return true;
+				} else {
+					return 'Please enter a valid Jira number';
+				}
+			},
+		},
+		{
+			type: 'input',
+			name: 'description',
+			message: 'Detailed description:',
+		},
+		{
 			type: 'confirm',
-			message: 'Push changes?',
-			name: 'push',
+			name: 'breakingChange',
+			default: false,
+			message: 'Is it a breaking change?',
+		},
+		{
+			type: 'confirm',
+			name: 'addFiles',
+			message: 'Add all files automatically?',
 		},
 	]).then((answers) => {
-		if (answers.push) {
-			execSync('git push', { stdio: 'inherit' });
+		if (answers.breakingChange) {
+			console.log('Breaking Change Not Implemented');
 		}
+
+		const scope = answers.specificScope
+			? answers.specificScope
+			: answers.scope;
+		const description = answers.description || '';
+
+		const commitCommand = `git commit -m "${answers.type}(${scope}): ${answers.message}" -m "${description}" -m "FS-${answers.jiraNumber}"`;
+
+		if (answers.addFiles) {
+			execSync('git add .', { stdio: 'inherit' });
+		}
+
+		try {
+			execSync(commitCommand, { stdio: 'inherit' });
+		} catch (e) {
+			console.error(e.message);
+			process.exit(1);
+		}
+
+		prompt([
+			{
+				type: 'confirm',
+				message: 'Push changes?',
+				name: 'push',
+			},
+		]).then((answers) => {
+			if (answers.push) {
+				execSync('git push', { stdio: 'inherit' });
+			}
+		});
 	});
-});
+}
